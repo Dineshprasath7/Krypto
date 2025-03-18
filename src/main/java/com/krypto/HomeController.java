c3d7ee2c0ee67c66b49fa6afaa6d5221bb6b5ce0
Dineshprasath <161580433+Dineshprasath7@users.noreply.github.com>
2025-03-18 07:16:38 +0530

    Initial Commit

---
+++ src/main/java/com/krypto/HomeController.java
@@ -0,0 +1,13 @@
+package com.krypto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String Home() {
+        return "Welcome to Krypto";
+    }
+}
